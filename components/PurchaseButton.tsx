c8c3501487106201627e6b840786ba23f117b756
Drew <chukwukaenudeme@gmail.com>
2026-04-05 04:17:10 +0100

    feat: implement createCheckoutSession action for Stripe checkout and update PurchaseButton to handle purchases

--- components/PurchaseButton.tsx
+++ components/PurchaseButton.tsx
@@ -2,7 +2,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import { Button } from "./ui/button";
-import { useQuery } from "convex/react";
+import { useAction, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Loader2Icon } from "lucide-react";
@@ -10,6 +10,8 @@ import { useState } from "react";
 
 const PurchaseButton = ({ courseId }: { courseId: Id<"courses"> }) => {
   const { user } = useUser();
+
+  const createCheckoutSession = useAction(api.stripe.createCheckoutSession);
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -29,7 +31,28 @@ const PurchaseButton = ({ courseId }: { courseId: Id<"courses"> }) => {
       : "skip",
   ) || { hasAccess: false };
 
-  const handlePurchase = () => {};
+  const handlePurchase = async () => {
+    if (!user) {
+      return alert("Please login to purxhase");
+    }
+    setIsLoading(true);
+
+    try {
+      const { checkoutUrl } = await createCheckoutSession({ courseId });
+
+      if (checkoutUrl) {
+        window.location.href = checkoutUrl;
+      } else {
+        throw new Error("Failed to create checkout session");
+      }
+    } catch (error) {
+      // todo: handle error
+
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   //   if user has not bought any course or is not subscribed
   if (!userAccess.hasAccess) {
